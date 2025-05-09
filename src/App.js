1e5e16935e4b776b559c603006ac49edfb3cce85
Titilope Balikis <titilopebalikis806@gmail.com>
2025-05-09 16:41:18 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+        <header></header>
+        <nav></nav>
+        <main></main>
+        <footer></footer>
+    </>
+  );
+}
+
+export default App;
